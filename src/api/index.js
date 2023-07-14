7335d77e2d474a5cf3066086b702399fc29cf03a
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-07-14 17:07:55 +0530

    Creating custom fetch function

---
+++ src/api/index.js
@@ -0,0 +1,40 @@
+
+//global fetch function to make api calls
+const customFetch =  async (url, {body, ...customConfig}) => {
+  const headers = {
+    'content-type': 'application/json; charset=UTF-8'
+  }
+
+  //define the configuration for fetch function
+  const config = {
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers
+    }
+  }
+
+  //if body exists add it to config
+  if(body){
+    config.body = getFormBody(body);
+  }
+
+  try{
+    //get response from api
+    const response = await fetch(url, config);
+    const data = await response.json();
+
+    //if response is successful return the data
+    if(data){
+      return {
+        data: data,
+        success: true
+      }
+    }
+  }catch(error){
+    return {
+      message: error,
+      success: false
+    }
+  }
+}
